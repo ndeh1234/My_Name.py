5ec0424292b4f82535972d678921ddf7b6a29b2d
ndeh1234 <30512965+ndeh1234@users.noreply.github.com>
2019-09-04 20:59:04 -0500

    Add files via upload

---
+++ My_Name.py
@@ -0,0 +1,6 @@
+name = input('Enter your name:')  # Getting input (name) from user
+month = input('Enter the month you were born in :') # Getting input (month) from user
+print('Hi!!, ' + name) # Printing a greeting message and user's name
+print('Your name has length {0}'.format(len(name))) # Printing length of user's name
+print('Your Happy birthday month is {0}'.format(month)) # Printing a happy birth day month message
+
